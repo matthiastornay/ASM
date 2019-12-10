f0e61e67a03cf1d21108b0da4a4f8a59764fb419
Matthias Tornay <47396178+matthiastornay@users.noreply.github.com>
2019-12-10 08:53:18 +0100

    Update vector_sum.s

--- First steps/vector_sum.s
+++ First steps/vector_sum.s
@@ -1,21 +1,23 @@
 .data
 
-vector: .int 25,-5,60,20
+vector: .int 25, -5, 60, 20
 result:	.int 0
 
 .text
 .global main
 
-main:		ldr r0, =vector
-			mov r1, #0
-			mov r2, #0
-loop:		add r1, r1, #1
-        	ldr r3, [r0]
-        	add r2, r2, r3
-        	add r0, r0, #4
-test_loop: 	cmp r1, #4
-        	blt loop
-        	str r2, [r0]
-			bx lr
+main:	LDR r0, =vector
+	MOV r1, #0
+	MOV r2, #0
+	
+loop:	ADD r1, r1, #1
+	LDR r3, [r0]
+	ADD r2, r2, r3
+	ADD r0, r0, #4
+		
+test_loop:	CMP r1, #4
+		BLT loop
+		STR r2, [r0]
+		BX lr
 
 .end

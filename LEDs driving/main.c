64cde9540fb8a95efd77eaa0a5a78b16cdd70eec
Matthias Tornay <47396178+matthiastornay@users.noreply.github.com>
2019-12-10 09:34:23 +0100

    Update main.c

--- LEDs driving/main.c
+++ LEDs driving/main.c
@@ -8,22 +8,24 @@ void main(void) {
 
 	int i, j, value;
 
-	for(i=1; i<10; i++) {
+	for(i = 1; i < 10; i++) {
 
 		value = 256;
 
-		for(j=0; j<4; j++)
-		{
-			Led_on(value<<j);
-			wait(OPERATIONS*i);
+		for(j = 0; j < 4; j++) {
+			
+			Led_on(value << j);
+			wait(OPERATIONS * i);
+			
 		}
 
 		value = 32768;
 
-		for(j=0; j<4; j++)
-		{
-			Led_on(value>>j);
-			wait(OPERATIONS*i);
+		for(j = 0; j < 4; j++) {
+			
+			Led_on(value >> j);
+			wait(OPERATIONS * i);
+			
 		}
 
 	}
@@ -31,9 +33,11 @@ void main(void) {
 	Led_on(0);
 	wait(1000000);
 
-	for(i=0; i<=32768; i+=16){
+	for(i = 0; i <= 32768; i += 16) {
+		
 		Led_on(i);
 		wait(10000);
+		
 	}
 
 	Led_on(0);
@@ -41,4 +45,3 @@ void main(void) {
 	return;
 
 }
-

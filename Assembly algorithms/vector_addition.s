471c3e3f948b3f76fca2fa14205145b74ceaad1c
Matthias Tornay <47396178+matthiastornay@users.noreply.github.com>
2019-12-10 09:00:06 +0100

    Update vector_addition.s

--- Assembly algorithms/vector_addition.s
+++ Assembly algorithms/vector_addition.s
@@ -4,33 +4,33 @@
 
 .data
 
-matrix1:	.byte 165,67,107,67,236,93,20,80,80,77,59,158,51,164,69,20
-matrix2:	.byte 195,65,224,205,136,83,217,130,23,27,20,10,61,166,39,30
+matrix1:	.byte 165, 67, 107, 67, 236, 93, 20, 80, 80, 77, 59, 158, 51, 164, 69, 20
+matrix2:	.byte 195, 65, 224, 205, 136, 83, 217, 130, 23, 27, 20, 10, 61, 166, 39, 30
 result:		.long 0
 
 .text
 .global main
 
-main:	ldr r1, =matrix1
-		ldr r2, =matrix2
-		ldr r0, =result
+main:	LDR r1, =matrix1
+	LDR r2, =matrix2
+	LDR r0, =result
 
-		mov r5, #0
-		mov r6, #0
+	MOV r5, #0
+	MOV r6, #0
 
-loop:	cmp r6, #N
-		bgt end
-		ldrb r3, [r1]
-		ldrb r4, [r2]
-		add r5, r3, r4
-		str r5, [r0]
-		add r1, r1, #1
-		add r2, r2, #1
-		add r0, r0, #1
-		add r6, r6, #1
-		b loop
+loop:	CMP r6, #N
+	BGT end
+	LDRB r3, [r1]
+	LDRB r4, [r2]
+	ADD r5, r3, r4
+	STR r5, [r0]
+	ADD r1, r1, #1
+	ADD r2, r2, #1
+	ADD r0, r0, #1
+	ADD r6, r6, #1
+	B loop
 
-end:	bx lr
+end:	BX lr
 
 .end
 

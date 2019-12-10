54d5217b07b9f34a4cc13fe49d2363c8cb5428fd
Matthias Tornay <47396178+matthiastornay@users.noreply.github.com>
2019-12-10 09:02:22 +0100

    Update trial_division_primality_test.s

--- Assembly algorithms/trial_division_primality_test.s
+++ Assembly algorithms/trial_division_primality_test.s
@@ -1,5 +1,5 @@
-// Trial divison, adapted from the C script linked
-// in the report.
+// Trial divison, adapted from a C program
+// Very SLOW and not optimized !
 
 .equ N, 251
 
@@ -7,44 +7,43 @@
 .global main
 
 main: 	MOV r0, #N
-		MOV r6, #1
-		AND r0, r0, r6
-		EOR r0, r0, r6 // Logical NOT
+	MOV r6, #1
+	AND r0, r0, r6
+	EOR r0, r0, r6 // Logical NOT
 
-		MOV r7, #N
+	MOV r7, #N
 
-		MOV r2, #3
+	MOV r2, #3
 
-		CMP r0, #1
-		BEQ return_2
-		CMP r7, #2
-		BLT return_2
+	CMP r0, #1
+	BEQ return_2
+	CMP r7, #2
+	BLT return_2
 
-		UDIV r3, r7, r2
+	UDIV r3, r7, r2
 
 loop_start:	CMP r2, r3
-			BGT return_true
+		BGT return_true
 
 do_modulo:	SUB r5, r7, r2
-			ADD r2, #1
-			CMP r5, r2
-			BGT do_modulo
+		ADD r2, #1
+		CMP r5, r2
+		BGT do_modulo
 
 loop_end:	MVN r5, r5
-			CMP r5, #1
-			BEQ return_false
-			ADD r2, r2, #2
-			B loop_start
+		CMP r5, #1
+		BEQ return_false
+		ADD r2, r2, #2
+		B loop_start
 
 return_2:	CMP r7, #2
-			BEQ return_true
-			B return_false
+		BEQ return_true
+		B return_false
 
 return_true:	MOV r1, #1
-				B end
+		B end
 
 return_false:	MOV r1, #0
-				B end
+		B end
 
 end:	BX lr
-
